1f0080fbd1619d0bb8018d31af579d4b0a7ec5ad
joaosoutto95 <joaosoutto95@gmail.com>
2024-02-14 09:28:53 -0300

    argparser -d do not send email

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
+import argparse
+
 from configs import *
 from utils import *
 from manage_db import *
+
+parser = argparse.ArgumentParser(description='Process and send surfing forecast data.')
+parser.add_argument('-d', '--disable_email', action='store_true', help='Disable sending email')
+args = parser.parse_args()
 
 local = NOME_PRAIA + ', ' + ESTADO
 data_json = stormglass_request(local)
@@ -16,9 +22,10 @@ if data_json:
     df_forc_1day, df_tides_1day, excel_name = save_excel(df_forc, df_tid)
 
     send_forecast_to_db(df_forc_1day, df_tides_1day)
-
-    send_email(file_path=f'./{excel_name}', file_name=excel_name)
-
+    if not args.disable_email:
+        send_email(file_path=f'./{excel_name}', file_name=excel_name)
+    else:
+        Path(f'./{excel_name}').unlink()
 else:
     print('API Request failed.')
 
